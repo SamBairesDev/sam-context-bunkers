dfea612cb1cc367c9bcad5fa96c82451a1871894
Sam Palumbo <sampalumbo@Sams-MacBook-Pro.local>
2021-10-21 18:29:54 -0300

    fix join table orders vapers

--- db/migrate/20211021211644_create_join_table_orders_vapers.rb
+++ db/migrate/20211021211644_create_join_table_orders_vapers.rb
@@ -3,6 +3,7 @@ class CreateJoinTableOrdersVapers < ActiveRecord::Migration[6.1]
     create_join_table :orders, :vapers do |t|
       # t.index [:order_id, :vaper_id]
       # t.index [:vaper_id, :order_id]
+      t.integer :quantity
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_21_210741) do
+ActiveRecord::Schema.define(version: 2021_10_21_211644) do
 
   create_table "accessories", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2021_10_21_210741) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "liquids_orders", id: false, force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "liquid_id", null: false
+    t.integer "quantity"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.boolean "open"
     t.datetime "date_opened"
@@ -49,6 +55,12 @@ ActiveRecord::Schema.define(version: 2021_10_21_210741) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "orders_vapers", id: false, force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "vaper_id", null: false
+    t.integer "quantity"
   end
 
   create_table "users", force: :cascade do |t|
