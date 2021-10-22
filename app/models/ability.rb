e6d5a1e4660376164a0b1b6257b3c4652b4473d3
Sam Palumbo <sampalumbo@Sams-MacBook-Pro.local>
2021-10-22 09:36:45 -0300

    access tweeks on views

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
       can :manage, :all
     else
       can :read, :all
+      can [:read, :create, :update, :destroy], Order
     end
     #
     # The first argument to `can` is the action you are giving the user
